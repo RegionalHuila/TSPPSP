fb6309e3facc348c0456203d91604599f24fa655
luis henry <lhreyes5@misena.edu.co>
2018-09-01 14:39:07 -0500

    1231

--- app/src/main/java/com/example/worldskills/tsppsp/SplashActivity.java
+++ app/src/main/java/com/example/worldskills/tsppsp/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //decorar vista
+        View decorar = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorar.setSystemUiVisibility(uiOptions);
+
 // creamos un salto de vista de 2 segundos al menu activity
         new Handler().postDelayed(new Runnable() {
             @Override
