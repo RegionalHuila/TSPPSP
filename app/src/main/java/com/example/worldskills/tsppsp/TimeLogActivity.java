97feb6b2dc9ebc60a4c49aa9de26ac3ca0f1a7cc
Brahiam Musse <bamusse@misena.edu.co>
2018-09-01 12:57:23 -0500

    10

--- app/src/main/java/com/example/worldskills/tsppsp/TimeLogActivity.java
+++ app/src/main/java/com/example/worldskills/tsppsp/TimeLogActivity.java
@@ -9,8 +9,10 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -30,21 +32,24 @@ public class TimeLogActivity extends AppCompatActivity {
         etHoraFinal = (EditText)findViewById(R.id.etHoraFinal);
         etHoraInicio = (EditText)findViewById(R.id.etHoraInicio);
         etInterrupcion = (EditText)findViewById(R.id.etMinutosInterrupcion);
-        btnFinal = (Button)findViewById(R.id.btnHoraInicio);
+        btnInicio = (Button)findViewById(R.id.btnHoraInicio);
         btnFinal = (Button)findViewById(R.id.btnHoraFinal);
         btnRegistrar = (Button)findViewById(R.id.btnResgistrar);
         spFase = (Spinner)findViewById(R.id.spFase) ;
 
         ArrayAdapter<CharSequence> adaptador =  ArrayAdapter.createFromResource(this,R.array.Fases, android.R.layout.simple_list_item_1);
         spFase.setAdapter(adaptador);
-
         btnInicio.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                SimpleDateFormat formato = new SimpleDateFormat();
+               SimpleDateFormat formato = new SimpleDateFormat("yyyy MM d");
+
+
 
             }
         });
+
 
 
 
