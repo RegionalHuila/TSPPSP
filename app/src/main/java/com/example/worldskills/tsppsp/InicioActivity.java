d3cb767d894b54c54f8bf4941849a038235e0eba
Brahiam Musse <bamusse@misena.edu.co>
2018-09-01 07:59:35 -0500

    1

--- app/src/main/java/com/example/worldskills/tsppsp/InicioActivity.java
+++ app/src/main/java/com/example/worldskills/tsppsp/InicioActivity.java
@@ -9,5 +9,7 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        //musse
     }
 }
