c5f174fce197271d89d159d2929c36e80c2eee3d
luis henry <lhreyes5@misena.edu.co>
2018-09-01 08:42:49 -0500

    henry 2

--- app/src/main/java/com/example/worldskills/tsppsp/MenuActivity.java
+++ app/src/main/java/com/example/worldskills/tsppsp/MenuActivity.java
@@ -80,17 +80,11 @@ public class MenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_time_log) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_defect_log) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_proyect_plan_summary) {
 
         }
 
